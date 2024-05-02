9d88b9a245e0c97f14bdf16f6b2a0362d5b4be76
pratham798 <prathamjamnal7982@gmail.com>
2024-05-02 12:56:46 +0530

    feat: add api for fetching jobs

---
+++ src/app/Api/Jobs.js
@@ -0,0 +1,20 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+/**
+ * Asynchronously searches for jobs by making a POST request to the specified API endpoint.
+ * @returns {Promise<Array>} A promise that resolves to an array of job objects.
+ * @throws {Error} If an error occurs during the search process.
+ */
+async function searchJobs(requestOptions) {
+  try {
+    const response = await fetch('https://api.weekday.technology/adhoc/getSampleJdJSON', requestOptions);
+    const jobs= await response.json();
+    return jobs;
+  } catch(error){
+    return error;
+  }
+}
+
+export default {
+  searchJobs
+};
